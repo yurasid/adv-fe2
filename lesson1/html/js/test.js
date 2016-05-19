d387c8ad3dcf07c2cb0f684777921aaca942ad9a
Yurii Sydoriak <yura.sid@gmail.com>
2016-05-19 18:03:53 +0300

    Add odd and even modificators. Set classes from js

--- lesson1/html/js/test.js
+++ lesson1/html/js/test.js
@@ -15,18 +15,23 @@ var $rawJsonDomContainer = $('.raw-json'),
 
   // table block helper
   Handlebars.registerHelper('table', function (ctx, options) {
-    return '<div class=description-container>'
-        + ctx.map( function(e, i) {
-            var style = 'style="padding: 20px";';
-            if ( i%2 ) {
-                style += ';background: #ccc';
-            }
-            style += ':"';
-            return '<div class="description"' + style + '>' + options.fn( ctx[i] ) + '</div>';
-        } ).join('\n') + '</div>';
-    // return new Handlebars.SafeString( ctx.reduce( function( resultString, element, i ) {
-    //   return resultString + '<div class="description">' + options.fn( ctx[i] ) + '</div>';
-    // }, '<div class="description-container">') + '</div>');
+    // return '<div class=description-container>'
+    //     + ctx.map( function(e, i) {
+    //         var style = 'style="padding: 20px";';
+    //         if ( i%2 ) {
+    //             style += ';background: #ccc';
+    //         }
+    //         style += ':"';
+    //         return '<div class="description"' + style + '>' + options.fn( ctx[i] ) + '</div>';
+    //     } ).join('\n') + '</div>';
+
+    return new Handlebars.SafeString( ctx.reduce( function( resultString, element, i ) {
+        var tableModifier = i % 2 ? 'even' : 'odd';
+
+        return resultString + '<div class="description description_' + tableModifier +'">' + options.fn( ctx[i] ) + '</div>';
+
+    }, '<div class="description-container">') + '</div>');
+
   } );
 
   // add changes to the DOM
