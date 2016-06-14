5be2024a33f4451585e35448220ad0543f26233a
Yurii Sydoriak <yura.sid@gmail.com>
2016-06-14 12:00:43 +0300

    Working

--- lesson06/client_src/containers/game/game.js
+++ lesson06/client_src/containers/game/game.js
@@ -5,34 +5,20 @@ var GodGiftForm = require( 'containers/god-gift-form/god-gift-form.js' ),
 module.exports = function Game() {
   var elem = $( '<div></div>' ),
     resourcePromise = fetch( '/json-server/wealth' )
-      .then( r => {
-        return r.json();
-      } )
-      .then( j => {
-
-        var resources = j.resources.map( r => new Resource( r ) ),
-          giftForm = new GodGiftForm({ resources: resources }),
-          userWealth = new UserWealth({ resources: resources });
-        // create resources
-        // e.g {count: 10, name: gold}
-
-        // create GodGiftForm
-        // {resources: resources}
-
-        // create UserWealth
-        // {resources: resources}
-
-        return { 'ggf': giftForm, 'uw': userWealth };
-      } );
+      .then( r => r.json() )
+      .then( j => j.resources.map( r => new Resource( r ) ) )
+      .catch( e => console.log( 'Error in Game constructor ', e ) );
 
   function render() {
     elem.html( App.templates[ 'game' ]( {} ) );
-    elem.find( '.game__god-gift-form' ).html( resourcePromise.then( function( r ) {
-      r.ggf.render().elem;
-    } ) );
-    elem.find( '.game__wealth' ).html( resourcePromise.then( function( r ) {
-      r.uw.render().elem;
-    } ) );
+    resourcePromise.then( r => {
+
+      elem.find( '.game__god-gift-form' )
+        .html( new GodGiftForm( { resources: r } ).render().elem );
+      elem.find( '.game__wealth' )
+        .html( new UserWealth( { resources: r } ).render().elem );
+    } )
+    .catch( e => console.log( 'Error in render() function ', e ) );
     return this;
   }
 
